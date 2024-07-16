7d8eebfbb3078bdf570a6d89261c8f96fc05c674
Julio Solano <juliocesarsolanogarcia@gmail.com>
2024-07-15 21:10:47 -0500

    Incluye imagenes pequeñas y textos en Tradición... y en Nuestros...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { PublicationComponent } from './components/publication/publication.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { NewsComponent } from './components/news/news.component';
+import { EventsComponent } from './components/events/events.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    PublicationComponent,
+    NavbarComponent,
+    SliderComponent,
+    NewsComponent,
+    EventsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
